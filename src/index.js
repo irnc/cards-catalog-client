7f0cf41f929f27e375c5f1c0fc505ff2360cd9e1
Mikutsevich Anton <mikutsevich.anton@gmail.com>
2017-04-05 02:18:02 +0300

    Remove mocked data and Add real data

--- src/index.js
+++ src/index.js
@@ -1,16 +1,3 @@
-/*
-import index from './index.jsx';
-
-const getData = require('./getData.js');
-const render = require('./index.jsx');
-
-const dataPromise = getData();
-
-dataPromise.then(function (data) {
-  render(data);
-});
-*/
-
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
