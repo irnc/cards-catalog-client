6fe513c38fc445418154d498d49c518c0de67868
Mikutsevich Anton <mikutsevich.anton@gmail.com>
2017-04-05 01:38:24 +0300

    Create Card component

--- src/App.js
+++ src/App.js
@@ -2,16 +2,50 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+function Card(props) {
+    return (
+        <div className="card">
+            <img src={props.imageUrl} />
+            <h1>{props.name}</h1>
+            <ul>
+                <li>Phone: {props.contacts.phone}</li>
+                <li>Email: {props.contacts.email}</li>
+            </ul>
+        </div>
+    );
+}
+
+var cards = [
+    {
+        name: "name1",
+        contacts: {
+            phone: "ph1",
+            email: "em1"
+        }
+    },
+    {
+        name: "name2",
+        contacts: {
+            phone: "ph2",
+            email: "em2"
+        }
+    },
+];
+
 class App extends Component {
   render() {
     return (
       <div className="App">
         <div className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+          <h2>Welcome to bad solutions</h2>
         </div>
         <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
+          {
+              cards.map ((card) => {
+                  return <Card name={card.name} contacts={card.contacts} />
+              })
+          }
         </p>
       </div>
     );
