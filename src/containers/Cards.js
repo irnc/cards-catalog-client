04ae07e1d3d9e94530a127a196f56f3619ba5e60
Pavel Zubkou <pavel.zubkou@gmail.com>
2017-04-05 14:03:56 +0300

    Add example of a container component

--- src/containers/Cards.js
+++ src/containers/Cards.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import Card from "../components/Card";
 
-
+// TODO move to components
 function Cards(props) {
     return (
         <div className="cards">
@@ -14,4 +14,31 @@ function Cards(props) {
     );
 }
 
-export default Cards;
+class CardsRoute extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = { loaded: false };
+    }
+    
+    componentDidMount() {
+        fetch('https://httpbin.org/get').then(res => res.json()).then(body => {
+            this.setState({
+                loaded: true,
+                cards: [
+                    { card: 1, headers: body.headers }
+                ]
+            });
+        });
+    }
+    
+    render() {
+        if (!this.state.loaded) {
+            return <div>Not loaded</div>;
+        }
+        
+        return <Cards cards={this.state.cards} />;
+    }
+}
+
+export default CardsRoute;
+export default connect(mapS, mapD)(Cards)
